da845d54765b5e13b0edddde551868b0bb2faefb
Hannes Schütz <hannes@fam-schuetz.de>
2018-07-23 15:59:43 +0200

    basic setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,22 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'TVO Kasse',
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sass',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "TVO Kasse",
+        short_name: "TVO Kasse",
+        start_url: "/",
+        background_color: "#f7f0eb",
+        theme_color: "#a2466c",
+        display: "minimal-ui",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+      },
+    },
+    'gatsby-plugin-offline',
+    ],
 }
