bd521d12a21dfec3446990e718f3487adc07442d
Hannes Schütz <hannes@fam-schuetz.de>
2018-07-23 16:16:29 +0200

    change display mode

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,9 +11,9 @@ module.exports = {
         name: "TVO Kasse",
         short_name: "TVO Kasse",
         start_url: "/",
-        background_color: "#f7f0eb",
-        theme_color: "#a2466c",
-        display: "minimal-ui",
+        background_color: "#121212",
+        theme_color: "#121212",
+        display: "standalone",
         //icon: "src/images/icon.png", // This path is relative to the root of the site.
       },
     },
