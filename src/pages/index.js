8409e223c2362d2b9c462859c8a6b7249ca4177c
Hannes Schütz <hannes@fam-schuetz.de>
2018-07-30 11:12:00 +0200

    chores

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,5 @@
 import React from 'react'
-import Link from 'gatsby-link'
-import Price from '../components/Price'
-import VoucherContainer from '../components/VoucherContainer'
 import MainPage from '../components/MainPage'
-import Shortid from 'shortid'
 
 
 const IndexPage = () => (
