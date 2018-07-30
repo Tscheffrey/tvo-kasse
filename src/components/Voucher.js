8fe4c889542d50da8d6facaf306ed3cb771b5ab7
Hannes Schütz <hannes@fam-schuetz.de>
2018-07-30 13:04:52 +0200

    minor stye changes

--- src/components/Voucher.js
+++ src/components/Voucher.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 const VoucherWrapper = styled.div`
-  height: 200px;
+  height: auto;
   width: 33%;
   padding: 8px;
 `
@@ -13,20 +13,26 @@ const VoucherInner = styled.div`
   background: ${props => props.bgcolor ? props.bgcolor : 'grey' };
   cursor: pointer;
   user-select: none;
-  padding: 32px;
+  padding: 16px;
   border-radius: 5px;
   overflow: hidden;
   position: relative;
   box-shadow: 0 0 30px 6px rgba(32,31,36,.11);
-  transition: transform 100ms cubic-bezier(.01,.68,.33,1.44),box-shadow .5s cubic-bezier(.01,.68,.33,1.44);
+  transition: transform 100ms cubic-bezier(.01,.68,.33,1.44),box-shadow 100ms cubic-bezier(.01,.68,.33,1.44);
   :active {
-    box-shadow: 0 0 40px 13px rgba(32,31,36,.11);
-    transform: scale(1.01);
+    box-shadow: none;
+    transform: scale(0.99);
   }
 `
 
 const Title = styled.span`
   font-size: 24px;
+  font-family: 'Roboto Condensed'
+  font-weight: 700;
+`
+
+const Subtitle = styled.span`
+  font-size: 12px;
   font-family: 'Roboto Condensed'
 `
 
@@ -36,9 +42,22 @@ const TitleContainer = styled.span`
   align-items: baseline;
 `
 
+const SubtitleContainer = styled.span`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+`
+
 const Price = styled.span`
   font-family: 'Roboto Mono', monospace;
   font-size: 24px;
+  font-weight: 600;
+`
+
+const Deposit = styled.span`
+  font-family: 'Roboto Mono', monospace;
+  font-size: 24px;
+  font-weight: 300;
 `
 
 class Voucher extends React.Component {
@@ -51,8 +70,16 @@ class Voucher extends React.Component {
     if(this.props.onPress) this.props.onPress(this.props.item)
   }
 
+  formatPrice(price) {
+    return price.toLocaleString('de-DE',  {minimumFractionDigits: 2})
+  }
+
   totalPrice(){
-    return (this.props.item.price + this.props.item.deposit).toLocaleString('de-DE',  {minimumFractionDigits: 2})
+    return this.formatPrice(this.props.item.price + this.props.item.deposit)
+  }
+
+  deposit(){
+    return this.formatPrice(this.props.item.deposit)
   }
 
   render(){
@@ -63,7 +90,10 @@ class Voucher extends React.Component {
             <Title>{this.props.item.title}</Title>
             <Price>{this.totalPrice()}</Price>
           </TitleContainer>
-
+          <SubtitleContainer>
+            <Subtitle>{'test'}</Subtitle>
+            <Deposit>{this.deposit()}</Deposit>
+          </SubtitleContainer>
         </VoucherInner>
       </VoucherWrapper>
     )
