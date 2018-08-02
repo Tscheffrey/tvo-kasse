f7d19c8da9d6da73cc77a82a44bb8b385dfbc96b
Hannes Schütz <hannes@fam-schuetz.de>
2018-08-03 00:10:42 +0200

    style reset button on mobile

--- src/components/Price.js
+++ src/components/Price.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 import ResetIcon from '../images/loop.svg'
 import media from '../helpers/media'
+import Responsive from 'react-responsive'
+
+const MUp = props => <Responsive {...props} minWidth={576 - 1} />
+
 
 const Container = styled.div`
     background: ${props => props.primaryColor};
@@ -15,7 +19,11 @@ const Container = styled.div`
 const Value = styled.div`
     font-family: 'Roboto Mono', monospace;
     font-size: 80px;
-    ${media.down.m`font-size: 44px;`}
+    ${media.down.m`
+      font-size: 64px;
+      padding-top: 4px;
+      padding-bottom: 4px;
+      `}
     margin-right: 16px;
     font-weight: 500;
 `
@@ -42,8 +50,20 @@ const ResetButton = styled.div`
     height: 110px;
     width: 110px;
     ${media.down.m`
-          height: 58px;
-          width: 58px;
+        position: fixed;
+        bottom: 24px;
+        right: 24px;
+        border-radius: 50%;
+        height: 58px;
+        width: 58px;
+        z-index: 3;
+        box-shadow: 0px 4px 19px 8px rgba(47, 47, 47, 0.22);
+        background-color: #2546bf;
+        transition: transform 100ms cubic-bezier(.01,.68,.33,1.44), box-shadow 100ms cubic-bezier(.01,.68,.33,1.44);
+        :active {
+          box-shadow: 0 0 30px 6px rgba(47, 47, 47, 0);
+          transform: translateY(4px);
+        }
           `}
     background: #1f1f1f;
     cursor: pointer;
@@ -76,7 +96,9 @@ class Price extends React.Component {
           <AmountRight className='price-right' visible={rightSideVisible}>,{amountRight}</AmountRight>
           <Currency className='price-currency'  visible={amountIsNotZero}>{this.props.currency}</Currency>
         </Value>
-        <ResetButton onClick={this.props.onReset} />
+        {/* <MUp> */}
+          <ResetButton onClick={this.props.onReset} />
+        {/* </MUp> */}
       </Container>
     )
   }
