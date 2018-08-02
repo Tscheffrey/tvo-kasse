e71dd6ec88af13788543e305c654145cf73d087a
Hannes Schütz <hannes@fam-schuetz.de>
2018-08-02 23:26:47 +0200

    update vouchers

--- src/content/vouchers.js
+++ src/content/vouchers.js
@@ -3,14 +3,14 @@ import Shortid from 'shortid'
 const vouchers = [
   {
     title: 'Maultaschen gebr.',
-    color: '#BADA55',
+    color: '#e68568',
     price: 6.5,
     deposit: 2.5,
     depositMark: true,
   },
   {
     title: 'Maultaschen Brühe',
-    color: '#BADA55',
+    color: '#c9cab6',
     price: 5.5,
     deposit: 2.5,
     depositMark: true,
@@ -20,7 +20,7 @@ const vouchers = [
   },
   {
     title: 'Bier',
-    color: 'red',
+    color: '#fbee4f',
     price: 2.5,
     deposit: 2,
     others: [
@@ -30,7 +30,7 @@ const vouchers = [
   },
   {
     title: 'Kräusen',
-    color: 'green',
+    color: '#fff',
     price: 2.3,
     deposit: 2,
     depositMark: true,
@@ -39,25 +39,45 @@ const vouchers = [
     ],
   },
   {
+    title: 'Apfelschorle',
+    color: '#e9e7e7',
+    price: 2,
+    deposit: 0,
+    depositMark: true,
+    others: [
+      'Alkoholfrei',
+    ],
+  },
+  {
     title: 'Wein',
-    color: 'orange',
+    color: '#b0cb8b',
     price: 2.5,
     deposit: 2,
   },
   {
     title: 'Weinschorle',
-    color: 'orange',
+    color: '#fffba8',
     price: 2,
     deposit: 2,
   },
   {
     title: 'Cola',
-    color: 'orange',
+    color: '#c0deec',
     price: 1.5,
     deposit: 0,
     others: [
       'Sprudel',
       'MezzoMix',
+    ],
+  },
+  {
+    title: 'Portion Kartoffelsalat',
+    color: '#ec9696',
+    price: 1.5,
+    deposit: 0,
+    others: [
+      'leeres Glas',
+      'Schnaps',
     ],
   },
 ]
