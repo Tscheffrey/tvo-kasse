2eaddbaef7fb06f2ccc0c1c916b9d2a524418c0a
Hannes <hannes@fam-schuetz.de>
2020-01-07 11:26:57 +0100

    fix font widths

--- src/components/Voucher.js
+++ src/components/Voucher.js
@@ -11,25 +11,25 @@ const VoucherWrapper = styled.div`
   ${media.l`width: 50%;`}
   ${media.xl`width: 33.33%;`}
   padding: 8px;
-
 `
 
 const VoucherInner = styled.div`
   color: ${props => readableColor(props.bgcolor)};
   height: 100%;
   width: 100%;
-  background: ${props => props.bgcolor ? props.bgcolor : 'grey' };
+  background: ${props => (props.bgcolor ? props.bgcolor : 'grey')};
   cursor: pointer;
   user-select: none;
   padding: 16px;
   border-radius: 5px;
   overflow: hidden;
   position: relative;
-  box-shadow: 0 0 30px 6px  rgba(142, 142, 142, 0.5);
-  border: 1px solid rgba(0,0,0,0.1);
-  transition: transform 100ms cubic-bezier(.01,.68,.33,1.44), box-shadow 100ms cubic-bezier(.01,.68,.33,1.44);
+  box-shadow: 0 0 30px 6px rgba(142, 142, 142, 0.5);
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  transition: transform 100ms cubic-bezier(0.01, 0.68, 0.33, 1.44),
+    box-shadow 100ms cubic-bezier(0.01, 0.68, 0.33, 1.44);
   :active {
-    box-shadow: 0 0 30px 6px  rgba(142, 142, 142, 0);
+    box-shadow: 0 0 30px 6px rgba(142, 142, 142, 0);
     transform: translateY(6px);
   }
   ${media.down.m`
@@ -40,7 +40,7 @@ const VoucherInner = styled.div`
 const Title = styled.span`
   font-size: 28px;
   font-family: 'Roboto Condensed';
-  font-weight: 600;
+  font-weight: 700;
   ${ellipsis('250px')}
 `
 
@@ -64,7 +64,7 @@ const SubtitleContainer = styled.span`
 const Price = styled.span`
   font-family: 'Roboto Mono', monospace;
   font-size: 24px;
-  font-weight: 600;
+  font-weight: 700;
 `
 
 const Deposit = styled.span`
@@ -72,7 +72,7 @@ const Deposit = styled.span`
   font-size: 24px;
   font-weight: 300;
   white-space: nowrap;
-  opacity: ${props => props.visible ? 0.5 : 0 };
+  opacity: ${props => (props.visible ? 0.5 : 0)};
 `
 
 const DepositLabel = styled.span`
@@ -83,45 +83,52 @@ const DepositLabel = styled.span`
 `
 
 class Voucher extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props)
     this.onClick = this.onClick.bind(this)
   }
 
-  onClick(){
-    if(this.props.onPress) this.props.onPress(this.props.item)
+  onClick() {
+    if (this.props.onPress) this.props.onPress(this.props.item)
   }
 
   formatPrice(price) {
-    return price.toLocaleString('de-DE',  {minimumFractionDigits: 2})
+    return price.toLocaleString('de-DE', { minimumFractionDigits: 2 })
   }
 
-  totalPrice(){
+  totalPrice() {
     return this.formatPrice(this.props.item.price + this.props.item.deposit)
   }
 
-  deposit(){
+  deposit() {
     return this.formatPrice(this.props.item.deposit)
   }
 
-  render(){
-    let subtitle = this.props.item.others ? 'oder ' + this.props.item.others.join(', ') : ''
+  render() {
+    let subtitle = this.props.item.others
+      ? 'oder ' + this.props.item.others.join(', ')
+      : ''
     return (
       <VoucherWrapper>
-        <VoucherInner onMouseDown={this.onClick} bgcolor={this.props.item.color}>
+        <VoucherInner
+          onMouseDown={this.onClick}
+          bgcolor={this.props.item.color}
+        >
           <TitleContainer>
             <Title title={this.props.item.title}>{this.props.item.title}</Title>
             <Price>{this.totalPrice()}</Price>
           </TitleContainer>
           <SubtitleContainer>
             <Subtitle>{subtitle}</Subtitle>
-            <Deposit visible={!(this.props.item.deposit == 0)}><DepositLabel>Pfand</DepositLabel>{this.deposit()}</Deposit>
+            <Deposit visible={!(this.props.item.deposit == 0)}>
+              <DepositLabel>Pfand</DepositLabel>
+              {this.deposit()}
+            </Deposit>
           </SubtitleContainer>
         </VoucherInner>
       </VoucherWrapper>
     )
   }
-
 }
 
 export default Voucher
